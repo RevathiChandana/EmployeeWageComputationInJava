70db8706f06bb367b29190557bf0641f9121c1a7
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-22 23:19:44 +0530

    uc5:calulate wages for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,24 +1,24 @@
 public class EmployeeWage{
-	static int WagePerHr=20,FullDayHrs=8,PartTimeHrs=4;
+	static int totalsalary=0,WagePerHr=20,FullDayHrs=8,PartTimeHrs=4,NumberWorkingDays=20;
 	static double EmpWage;
-	static void CalculateWage(double EmpCheck){
+	static void CalculateWage(){
+	for(int day=1;day<=NumberWorkingDays;day++){
+                double EmpCheck=Math.floor(Math.random()*10)%3;
 		switch((int)EmpCheck){
-		case 1:	System.out.println("Employee is present and work full day");
-			EmpWage=WagePerHr*FullDayHrs;
+		case 1:	EmpWage=WagePerHr*FullDayHrs;
 			break;
-		case 2:	System.out.println("Emloyee is abscent and work part time");
-			EmpWage=WagePerHr*PartTimeHrs;
+		case 2:	EmpWage=WagePerHr*PartTimeHrs;
 			break;
-		default:System.out.println("employee is abscent");
-			EmpWage=0;
+		default:EmpWage=0;
 			break;
 		}
+		totalsalary=totalsalary+(int)EmpWage;
+	}
 	}
         public static void main(String[] args){
 		EmployeeWage Empobj=new EmployeeWage();
 		System.out.println("welcome to employee wage computation problem on master branch");
-                double empCheck=Math.floor(Math.random()*10)%3;
-                Empobj.CalculateWage(empCheck);
-		System.out.println("Emplayee wage: "+EmpWage);
+                Empobj.CalculateWage();
+		System.out.println("Emplayee totalsalary: "+totalsalary);
         }
 }
