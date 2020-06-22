b2b4c5ababd58bf0800189dff4a8572a28f6a34e
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-22 22:51:36 +0530

    uc3: Add part time employee and calculate wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,27 @@
 public class EmployeeWage{
-	static int WagePerHr=20,FullDayHr=8;
+	static int WagePerHr=20,FullDayHrs=8,PartTimeHrs=4;
 	static double EmpWage;
-	static void CalculateWage(double EmpCheck){
-		EmpWage=WagePerHr*FullDayHr*EmpCheck;
+	static void CalculateWage(double EmpCheck,int FullTime,int PartTime){
+		if(EmpCheck==FullTime){
+			System.out.println("Employee is present and work full day");
+			EmpWage=WagePerHr*FullDayHrs;
+		}
+		else if(EmpCheck==PartTime){
+			System.out.println("Emloyee is abscent and work part time");
+			EmpWage=WagePerHr*PartTimeHrs;
+		}
+		else{
+			System.out.println("employee is abscent");
+			EmpWage=0;
+		}
 	}
         public static void main(String[] args){
 		EmployeeWage Empobj=new EmployeeWage();
 		System.out.println("welcome to employee wage computation problem on master branch");
-                int IS_FULL_TIME=1;
-                double empCheck=Math.floor(Math.random()*10)%2;
-                if ( empCheck == IS_FULL_TIME )
-                        System.out.println("Emloyee is present");
-                else
-                        System.out.println("Employee is abscent");
-		Empobj.CalculateWage(empCheck);
+                int FullTime=2;
+		int PartTime=1;
+                double empCheck=Math.floor(Math.random()*10)%3;
+                Empobj.CalculateWage(empCheck,FullTime,PartTime);
 		System.out.println("Emplayee wage: "+EmpWage);
         }
 }
