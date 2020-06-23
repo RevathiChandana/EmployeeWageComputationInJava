9b3d1e0168c694b09ef1c5596422ea31a1c5cac4
Revathichandana30 <65756621+Revathichandana30@users.noreply.github.com>
2020-06-23 09:55:58 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,7 @@
 public class EmployeeWage{
 	static int WagePerHr=20,FullDayHrs=8,PartTimeHrs=4;
 	static double EmpWage;
-	static void CalculateWage(double EmpCheck,int FullTime,int PartTime){
+	public void CalculateWage(double EmpCheck,int FullTime,int PartTime){
 		if(EmpCheck==FullTime){
 			System.out.println("Employee is present and work full day");
 			EmpWage=WagePerHr*FullDayHrs;
